5fad71e28ccc50989ab85a2137e410184bdfed89
Tebogo <tebogomakoko@gmail.com>
2020-04-17 18:00:02 +0200

    Adding Project Structure for Clean Architecture Solution

---
+++ CleanArchitecture/WebUI.MVC/Data/Migrations/20200417154909_InitialMgration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebUI.MVC.Data.Migrations
+{
+    public partial class InitialMgration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
